b874f5ce80324bb43abaeef9aa2e4d0cee5a2011
danielsct56bm <danielsct56bm@gmail.com>
2022-11-06 23:00:32 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,4 +1,22 @@
 <?php 
+/*$contrasena = "";
+$usuario = "root";
+$nombre_bd = "crud1";
+
+try {
+	$bd = new PDO (
+		'mysql:host=localhost;
+		dbname='.$nombre_bd,
+		$usuario,
+		$contrasena,
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	);
+} catch (Exception $e) {
+	echo "Problema con la conexion: ".$e->getMessage();
+}
+*/?>
+
+<?php 
 $contrasena = "vt2Pp6@dx9Q4";
 $usuario = "grupo1";
 $nombre_bd = "grupo1";
